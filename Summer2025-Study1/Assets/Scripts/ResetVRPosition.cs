4886adbb7fb4f1d28d9f0f4088f35f3bd5319a80
John Rader <raderjohna@gmail.com>
2025-08-24 16:56:33 -0500

    reset position works, rotation not so

--- Summer2025-Study1/Assets/Scripts/ResetVRPosition.cs
+++ Summer2025-Study1/Assets/Scripts/ResetVRPosition.cs
@@ -6,7 +6,7 @@ public class ResetVRPosition : MonoBehaviour
 {
     public Transform starting_transform;
     public Transform xr_rig;
-    public Transform xr_camera;
+    public Transform xr_head;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,18 +16,17 @@ public class ResetVRPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(xr_head.eulerAngles.y);
     }
 
     public void ResetPos()
     {
-        Debug.Log(starting_transform.position);
-        Debug.Log(xr_camera.position);
-        xr_rig.localPosition = starting_transform.position - xr_camera.localPosition;
-        Debug.Log(xr_rig.position);
+        // reset position to origin point
+        Vector3 offset = xr_head.position - xr_rig.position;
+        xr_rig.position = starting_transform.position - offset;
         xr_rig.position = new Vector3(xr_rig.position.x, 0, xr_rig.position.z);
-        Debug.Log(xr_rig.position);
-        //xr_rig.rotation = Quaternion.Inverse(starting_transform.rotation) * xr_camera.rotation;
-        //xr_rig.eulerAngles = new Vector3(0, xr_rig.eulerAngles.y, 0);
+
+        //float new_angle = starting_transform.eulerAngles.y - xr_head.eulerAngles.y;
+        //xr_rig.eulerAngles = new Vector3(0, new_angle, 0);
     }
 }
