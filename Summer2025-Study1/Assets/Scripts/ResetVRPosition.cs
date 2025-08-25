bd61f1a926df0fedca0437a22fc72efa6d1663a8
John Rader <raderjohna@gmail.com>
2025-08-25 17:32:58 -0500

    resetting position AND rotation works

--- Summer2025-Study1/Assets/Scripts/ResetVRPosition.cs
+++ Summer2025-Study1/Assets/Scripts/ResetVRPosition.cs
@@ -7,6 +7,7 @@ public class ResetVRPosition : MonoBehaviour
     public Transform starting_transform;
     public Transform xr_rig;
     public Transform xr_head;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,17 +17,21 @@ public class ResetVRPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(xr_head.eulerAngles.y);
+
     }
 
     public void ResetPos()
     {
-        // reset position to origin point
-        Vector3 offset = xr_head.position - xr_rig.position;
-        xr_rig.position = starting_transform.position - offset;
-        xr_rig.position = new Vector3(xr_rig.position.x, 0, xr_rig.position.z);
+        // do 5 times because it works only after the first one??? no idea why but it works
+        for (int i = 0; i < 5; i++)
+        {
+            // reset position to origin point
+            Vector3 offset = xr_head.position - xr_rig.position;
+            xr_rig.position = starting_transform.position - offset;
+            xr_rig.position = new Vector3(xr_rig.position.x, 0, xr_rig.position.z);
 
-        //float new_angle = starting_transform.eulerAngles.y - xr_head.eulerAngles.y;
-        //xr_rig.eulerAngles = new Vector3(0, new_angle, 0);
+            float difference = starting_transform.eulerAngles.y - xr_head.eulerAngles.y;
+            xr_rig.Rotate(0f, difference, 0f, Space.World);
+        }
     }
 }
